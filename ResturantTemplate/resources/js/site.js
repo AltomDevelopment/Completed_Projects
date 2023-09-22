e8c891a7c343be149acfbdff2e7dfa4663fb5b11
Altomdevelopment <zack.altom@outlook.com>
2023-09-22 18:36:20 -0400

    Added Template

---
+++ ResturantTemplate/resources/js/site.js
@@ -0,0 +1,10 @@
+
+function showMenu() {
+    let obj = document.querySelector("#nav-menu"); 
+    if(obj.style.display == "none"){
+        obj.style.display = "block";
+    }
+    else{
+        obj.style.display = "none";
+    }   
+}
