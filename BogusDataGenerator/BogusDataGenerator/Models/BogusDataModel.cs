df6cdb886f887442c36546411ade521928e9aa63
Altomdevelopment <zack.altom@outlook.com>
2023-10-22 09:04:53 -0400

    Added Bogus Program

---
+++ BogusDataGenerator/BogusDataGenerator/Models/BogusDataModel.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_Template.Models;
+
+public class BogusDataModel
+{
+ public string ModelZipCode { get; set; }
+ public string ModelCity { get; set; }
+ public string ModelStreetAddress { get; set; }
+ public string ModelCityPrefix { get; set; }
+ public string ModelCitySuffix { get; set; }
+ public string ModelStreetName { get; set; }
+ public string ModelBuildingNumber { get; set; }
+ public string ModelStreetSuffix { get; set; }
+ public string ModelSecondaryAddress { get; set; }
+ public string ModelCounty { get; set; }
+ public string ModelCountry{ get; set; }
+ public string ModelFullAddress{ get; set; }
+ public string ModelCountryCode{ get; set; }
+ public string ModelState{ get; set; }
+ public string ModelStateAbbr{ get; set; }
+ public string ModelLatitude{ get; set; }
+ public string ModelLongitude{ get; set; }
+ public string ModelDirection{ get; set; }
+ public string ModelCardinalDirection{ get; set; }
+ public string ModelOrdinalDirection { get; set; }
+ public string ModelDepartment{ get; set; }
+ public string ModelPrice{ get; set; }
+ public string ModelCategories{ get; set; }
+ public string ModelProductName{ get; set; }
+ public string ModelProduct { get; set; }
+ public string ModelProductAdjective { get; set; }
+ public string ModelProductMaterial{ get; set; }
+ public string ModelEan8 { get; set; } 
+ public string ModelEan13{ get; set; }
+ public string ModelCompanySuffix{ get; set; }
+ public string ModelCompanyName{ get; set; }
+ public string ModelCatchPhrase{ get; set; }
+ public string ModelBs{ get; set; }
+ public string ModelColumn{ get; set; } 
+ public string ModelType{ get; set; }
+ public string ModelCollation{ get; set; } 
+ public string ModelEngine{ get; set; }
+ public string ModelPast{ get; set; }
+ public string ModelPastOffset{ get; set; }
+ public string ModelSoon{ get; set; }
+ public string ModelSoonOffset{ get; set; }
+ public string ModelFuture{ get; set; }
+ public string ModelFutureOffset{ get; set; } 
+ public string ModelBetween{ get; set; } 
+ public string ModelBetweenOffset{ get; set; }
+ public string ModelRecent{ get; set; }
+ public string ModelRecentOffset{ get; set; }
+ public string ModelTimespan{ get; set; }
+ public string ModelMonth{ get; set; } 
+ public string ModelWeekday{ get; set; } 
+ public string ModelAccount{ get; set; }
+ public string ModelAccountName{ get; set; }
+ public string ModelAmount{ get; set; }
+ public string ModelTransactionType{ get; set; }
+ public string ModelCurrency{ get; set; } 
+ public string ModelCreditCardNumber{ get; set; } 
+ public string ModelCreditCardCvv{ get; set; }
+ public string ModelBitcoinAddress{ get; set; }
+ public string ModelEthereumAddress{ get; set; }
+ public string ModelRoutingNumber{ get; set; } 
+ public string ModelBic{ get; set; }
+ public string ModelIban{ get; set; }
+ public string ModelAbbreviation{ get; set; }
+ public string ModelAdjective{ get; set; }
+ public string ModelNoun{ get; set; } 
+ public string ModelVerb{ get; set; } 
+ public string ModelIngVerb{ get; set; } 
+ public string ModelPhrase{ get; set; } 
+ public string ModelDataUri{ get; set; } 
+ public string ModelPicsumUrl{ get; set; } 
+ public string ModelPlaceholderUrl{ get; set; }
+ public string ModelLoremFlickrUrl{ get; set; } 
+ public string ModelLoremPixelUrl{ get; set; }
+ public string ModelAvatar{ get; set; }
+ public string ModelEmail{ get; set; }
+ public string ModelExampleEmail{ get; set; }
+ public string ModelUserName{ get; set; }
+ public string ModelUserNameUnicode{ get; set; }
+ public string ModelDomainName{ get; set; } 
+ public string ModelDomainWord{ get; set; } 
+ public string ModelDomainSuffix{ get; set; }
+ public string ModelIp{ get; set; }
+ public string ModelPort{ get; set; }
+ public string ModelIpAddress{ get; set; } 
+ public string ModelIpEndPoint{ get; set; }
+ public string ModelIpv6{ get; set; }
+ public string ModelIpv6Address{ get; set; } 
+ public string ModelIpv6EndPoint{ get; set; }
+ public string ModelUserAgent{ get; set; }
+ public string ModelMac{ get; set; } 
+ public string ModelPassword{ get; set; } 
+ public string ModelColor{ get; set; } 
+ public string ModelProtocol{ get; set; } 
+ public string ModelUrl { get; set; }
+ public string ModelUrlWithPath { get; set; }
+ public string ModelUrlRootedPath { get; set; }
+ public string ModelWord { get; set; }
+ public string ModelWords{ get; set; }
+ public string ModelLetter { get; set; }
+ public string ModelSentence { get; set; }
+ public string ModelSentences { get; set; }
+ public string ModelParagraph { get; set; }
+ public string ModelParagraphs { get; set; }
+ public string ModelText { get; set; }
+ public string ModelLines{ get; set; }
+ public string ModelSlug { get; set; }
+ public string ModelFirstName { get; set; }
+ public string ModelLastName { get; set; }
+ public string ModelFullName { get; set; }
+ public string ModelPrefix { get; set; }
+ public string ModelSuffix{ get; set; }
+ public string ModelFindName { get; set; }
+ public string ModelJobTitle{ get; set; }
+ public string ModelJobDescriptor { get; set; }
+ public string ModelJobArea { get; set; }
+ public string ModelJobType { get; set; }
+ public string ModelPhoneNumber { get; set; }
+ public string ModelPhoneNumberFormat{ get; set; }
+ public string ModelReview { get; set; }
+ public string ModelReviews { get; set; }
+ public string ModelSystem{ get; set; }
+ public string ModelFileName { get; set; }
+ public string ModelDirectoryPath { get; set; }
+ public string ModelFilePath { get; set; }
+ public string ModelCommonFileName { get; set; }
+ public string ModelMimeType { get; set; }
+ public string ModelCommonFileType { get; set; }
+ public string ModelCommonFileExt { get; set; }
+ public string ModelFileType { get; set; }
+ public string ModelFileExt { get; set; }
+ public string ModelSemver { get; set; }
+ public string ModelVersion { get; set; }
+ public string ModelException { get; set; }
+ public string ModelAndroidId{ get; set; }
+ public string ModelApplePushToken { get; set; }
+ public string ModelBlackBerryPin { get; set; }
+ public string ModelVin{ get; set; }
+ public string ModelManufacturer{ get; set; }
+ public string ModelModel{ get; set; }
+ public string ModelFuel { get; set; }
+ public string ModelNumber { get; set; }
+ public string ModelDigits { get; set; }
+ public string ModelEven { get; set; }
+ public string ModelOdd{ get; set; }
+ public string ModelDouble { get; set; }
+ public string ModelDecimal { get; set; }
+ public string ModelFloat { get; set; }
+ public string ModelByte{ get; set; }
+ public string ModelBytes{ get; set; }
+ public string ModelSByte { get; set; }
+ public string ModelInt { get; set; }
+ public string ModelUInt { get; set; }
+ public string ModelULong { get; set; }
+ public string ModelLong { get; set; }
+ public string ModelShort { get; set; }
+ public string ModelUShort { get; set; }
+ public string ModelChar { get; set; }
+ public string ModelChars { get; set; }
+ public string ModelString { get; set; }
+ public string ModelString2 { get; set; }
+ public string ModelHash { get; set; }
+ public string ModelBool { get; set; }
+ public string ModelReplaceNumbers { get; set; }
+ public string ModelReplaceSymbols{ get; set; }
+ public string ModelReplace { get; set; }
+ public string ModelClampString { get; set; }
+ public string ModelWordsArray { get; set; }
+ public string ModelGuid { get; set; }
+ public string ModelUuid { get; set; }
+ public string ModelRandomLocale{ get; set; } 
+ public string ModelAlphaNumeric{ get; set; }
+ public string ModelHexadecimal{ get; set; }
+}
