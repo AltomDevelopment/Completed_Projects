93e812a4051fd54e183c80401faf1ec0de2c26d5
Altomdevelopment <zack.altom@outlook.com>
2023-10-26 08:19:31 -0400

    Added Programs

---
+++ AppsCompletedInTutorials/SampleShoppingCart/asp.net-core-6-.net-6-project-shopping-cart-main/ShoppingCart/Infrastructure/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Management.Smo;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Infrastructure
+{
+        public class DataContext : IdentityDbContext<AppUser>
+        {
+                public DataContext(DbContextOptions<DataContext> options) : base(options)
+                {
+                }
+
+                public DbSet<Product> Products { get; set; }
+                public DbSet<Category> Categories { get; set; }
+        }
+}
